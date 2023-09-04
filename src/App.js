511606797e2a3f92d1f700b60d765434de588542
muhammad-suleman-awan <mmsulemanawan@gmail.com>
2023-09-04 11:32:05 +0500

    Db setup compete

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ import Des from "./component/Des";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Topbar from "./component/Topbar";
 import API from "./axios.jsx";
+import axios from "axios";
 // import AddAccount from "./component/AddAccount";
 function App() {
   const [myData, setMyData] = useState([]);
   const [isError, setIsError] = useState("");
   const [data, setData] = useState({});
 
-  // const [receivedData, setReceivedData] = useState('');
-  //call backfunction to receieved data from child
-  // const receievedDataFromChild = (data) =>{
-  //   setReceivedData(data);
-  //   console.log("receievedDataFromChild......... ",receivedData )
-  // }
+  const [receivedData, setReceivedData] = useState('');
+  // call backfunction to receieved data from child
+  const receievedDataFromChild = (data) =>{
+    setReceivedData(data);
+    console.log("receievedDataFromChild......... ",receivedData )
+  }
   const getData = () => {
     API.get("/show")
       .then((res) => {
@@ -37,23 +38,35 @@ function App() {
 
   const [receivedData1, setReceivedData1] = useState(null);
 
-  // const postData = () => {
+  const postData = () => {
 
-  //   API.post("/addProduct",data)
-  //     .then((response) => {
-  //       console.log("response", response);
-  //     })
-  //     .catch((err) => {
-  //       console.log("err", err);
-  //     });
-  // };
+    API.post("/addProduct",data)
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
 
  const handleChildData1 = (data) => {
-  //   setReceivedData1(data);
-  // //   console.log("Hello Data Received in Parents", data);
-  // postData();
+    setReceivedData1(data);
+  //   console.log("Hello Data Received in Parents", data);
+  postData();
   
  };
+
+//  const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const header = {"Access-Control-Allow-Origin":"*"}
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     axios.post('https://64b2533738e74e386d54f818.mockapi.io/form',{
+//       name:name,
+//       email:email,
+// header
+//     })
+//   };
 
   return (
     <div>
@@ -77,6 +90,48 @@ function App() {
         ></Des>
       </div>
     </div>
+
+
+  //   <div className="App">
+  //   <form>
+  //     <div className="mb-3">
+  //       <label for="exampleInputEmail1" className="form-label">
+  //         Name
+  //       </label>
+  //       <input
+  //         type="text"
+  //         className="form-control"
+  //         id="exampleInputEmail1"
+  //         aria-describedby="emailHelp"
+  //         onChange={(e) => setName(e.target.value)}
+  //       />
+  //       <div id="emailHelp" className="form-text">
+  //         We'll never share your name with anyone else.
+  //       </div>
+  //     </div>
+  //     <div className="mb-3">
+  //       <label for="exampleInputPassword1" className="form-label">
+  //         Email
+  //       </label>
+  //       <input
+  //         type="email"
+  //         className="form-control"
+  //         id="exampleInputPassword1"
+  //         onChange={(e) => setEmail(e.target.value)}
+  //       />
+  //     </div>
+  //     <button
+  //       type="submit"
+  //       onClick={handleSubmit}
+  //       className="btn btn-primary"
+  //     >
+  //       Submit
+  //     </button>
+  //   </form>
+  // </div>
+
+
+
   );
 }
 export default App;
